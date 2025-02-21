77a6962c9205b5e22dbb939cf34cdc75a5388f46
nsikhonde-1 <nsikhonde1@gmail.com>
2025-02-21 09:58:13 +0200

    Adding API integration

--- source/app.js
+++ source/app.js
@@ -1,14 +1,25 @@
 
-function generatePoem(event) {
-    event.preventDefault();
-
-    new Typewriter("#poem-text", {
-      strings: " When she first saw them, they were dazzling…",
-      autoStart: true,
-      delay: 1,
-      cursor: "",
-    });
+function showPoem(response) {
+   new Typewriter("#poem-text", {
+     strings: response.data.answer,
+     autoStart: true,
+     delay: 1,
+     cursor: "",
+   });
     
+}
+  
+function generatePoem(event) {
+  event.preventDefault();
+  
+  let poemInfo = document.querySelector(".text-form");
+  let apiKey = "713a631324o2dtc013aaf4374b846544";
+  let prompt = `Generate love poems about ${poemInfo.value}`;
+  let context = "You are a love expect who loves poems. You are to generate a 6 line love poems in basic HTML and separate lines in <br>. Do not include a title to the poem";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(showPoem);
+   
 }
 let poemForm = document.querySelector("#textForm");
 poemForm.addEventListener("submit", generatePoem);
